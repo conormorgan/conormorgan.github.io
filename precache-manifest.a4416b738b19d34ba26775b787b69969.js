89fdeda488ca6d592ac0845b2c0d8e3d32ec2e61
Conor <2199772m@student.gla.ac.uk>
2020-08-02 23:02:27 +0100

    Updates

---
+++ precache-manifest.a4416b738b19d34ba26775b787b69969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fa6fde972aa31ca5de63ad8aa91e67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f96451017a10aa73f2dd",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "12ac17453b8fabfc4e38",
+    "url": "/static/js/2.7155fb28.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/static/js/2.7155fb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96451017a10aa73f2dd",
+    "url": "/static/js/main.74899690.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  }
+]);
