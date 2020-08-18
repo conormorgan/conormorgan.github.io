ab4e61f1c5eaf4eb5c3bf24950733e3e1753a003
Conor <2199772m@student.gla.ac.uk>
2020-08-18 19:12:24 +0100

    added text about work and school

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -15,7 +15,7 @@ class Portfolio extends Component {
                 <div className="overlay">
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
+                    <p>{projects.description}</p>
                   </div>
                 </div>
               </a>
