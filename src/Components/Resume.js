0e63e614423c40b45f26d2add1c143d185c218c6
conormorgan <morgan.fourteen@hotmail.co.uk>
2022-02-11 21:04:49 +0000

    cut down paragraphs of text, updated some info too

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -16,18 +16,23 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
+      
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.descript_pt1}</p>
-        <p>{education.descript_pt2}</p>
+        <p>{education.desc1}</p>
+        <p>{education.desc2}</p>
+        <p>{education.desc3}</p>
         </div>
       })
+
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.descript_pt1}</p>
-            <p>{work.descript_pt2}</p>
+            <p>{work.desc1}</p>
+            <p>{work.desc2}</p>
+            <p>{work.desc3}</p>
+            
         </div>
       })
 
@@ -45,6 +50,17 @@ class Resume extends Component {
     return (
       <section id="resume">
 
+      <div className="row work">
+
+        <div className="three columns header-col">
+          <h1><span>Work</span></h1>
+        </div>
+
+        <div className="nine columns main-col">
+          {work}
+        </div>
+      </div>
+
       <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
@@ -60,16 +76,7 @@ class Resume extends Component {
       </div>
 
 
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
-        </div>
-    </div>
+      
 
 
 
